0d5c128abfbaadd54e65f4c27d1b1e361d2cc165
Kornel <33583478+Kraansponn@users.noreply.github.com>
2020-02-18 20:10:23 +0000

    buttons added

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,15 +4,29 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setWidth(500);
+        primaryStage.setHeight(300);
+
+        VBox dataEntry = new VBox(20);
+        Button add = new Button("add");
+        Button remove = new Button("remove");
+        Button list = new Button("list");
+        Button summary = new Button("summary");
+        Button load = new Button("load");
+        Button save = new Button("save");
+        Button exit = new Button("exit");
+        dataEntry.getChildren().addAll(add,remove,list,summary,load,save,exit);
+        primaryStage.setScene(new Scene(dataEntry));
         primaryStage.show();
     }
 
